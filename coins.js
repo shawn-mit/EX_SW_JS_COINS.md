a5fa7b3180b108f1ef3c38fe006322c19c5f13e8
Shawn M <forgeforza@outlook.com>
2016-10-30 21:30:14 -0500

    intial commit

---
+++ coins.js
@@ -0,0 +1,45 @@
+/*
+  Input: 0.67
+  Output:
+  {
+    quarters: 2,
+    dimes: 1,
+    nickels: 1,
+    pennies: 2
+  }
+
+*/
+
+var coinsWorth = {
+
+  quarters: 0.25,
+  dimes : 0.10,
+  nickels : 0.05,
+  pennies : 0.01,
+  coinCounter: function (money) {
+
+    var coinPurse ={};
+
+
+  coinPurse.quarters = money.floor(Math.) //money divided by the key of the object ;
+  money -=
+  coinPurse.dimes = 
+  coinPurse.nickels =  
+  coinPurse.pennies = 
+
+  return coinPurse;
+
+
+  }
+
+};
+
+//console.log(coinsWorth);
+
+  //money 'the orange argument' is the input of the user
+
+//function coinCounter (money) {
+  // Initialize a JavaScript object to hold the coins
+  
+var coins = coinCounter()
+console.log(coins);
